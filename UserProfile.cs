17837fef648ed3e3b10621572d54a60d5b509d91
Somnath Bera <somnathbera1919@gmail.com>
2023-05-25 18:04:42 +0530

    user profile edit page modifications

--- UserProfile.cs
+++ UserProfile.cs
@@ -11,13 +11,16 @@ namespace BeeTube
 {
     using System;
     using System.Collections.Generic;
-    
+    using System.ComponentModel.DataAnnotations;
+
     public partial class UserProfile
     {
         public int Id { get; set; }
         public string UserId { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
         public Nullable<System.DateTime> DOB { get; set; }
         public string Gender { get; set; }
         public string Country { get; set; }
